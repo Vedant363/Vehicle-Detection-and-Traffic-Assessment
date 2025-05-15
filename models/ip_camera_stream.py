86f255111dcb8795dccf1f95375409683e582c61
Vedant363 <vedant.ghumade363@gmail.com>
2025-05-16 00:45:10 +0530

    Improved IP camera stream reliability by specifying cv2.CAP_FFMPEG in VideoCapture

--- models/ip_camera_stream.py
+++ models/ip_camera_stream.py
@@ -4,7 +4,7 @@ def initialize_ip_camera_stream(ip_stream_url: str):
     if not ip_stream_url:
         raise ValueError("IP stream URL is empty.")
 
-    cap = cv2.VideoCapture(ip_stream_url)
+    cap = cv2.VideoCapture(ip_stream_url, cv2.CAP_FFMPEG)
 
     if not cap.isOpened():
         raise ValueError(f"Failed to open IP stream: {ip_stream_url}")
